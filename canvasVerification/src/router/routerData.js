6179a43510629762160e6687256d82281301f847
LeonardoDAna <943682226@qq.com>
2021-08-09 17:34:32 +0800

    router集成

--- canvasVerification/src/router/routerData.js
+++ canvasVerification/src/router/routerData.js
@@ -1,30 +1,49 @@
-export let datas = [{
+export let routerDatas = [{
     name: "验证码模块",
     Tag: ['canvas', "js"],
-    path: "/Carousel"
+    path: "/Carousel",
+    icon: "homePage",
+    component: () => import("@/components/Carousel.vue"),
+    label: "",
   }, {
     name: "日历组件",
     Tag: ["js"],
-    path: "/dateTools"
+    path: "/dateTools",
+    component: () => import("@/components/dateTools.vue"),
+    label: "",
   }, {
     name: "触底刷新",
-    Tag: ["js","elementUI"],
-    path: "/overflowAuto"
+    Tag: ["js", "elementUI"],
+    path: "/overflowAuto",
+    component: () => import("@/view/overflowAuto.vue"),
+    label: "",
   },
   {
     name: "放大镜",
-    Tag: ["js","dom"],
-    path: "/magnifyingGlass"
+    Tag: ["js", "dom"],
+    path: "/magnifyingGlass",
+    component: () => import("@/view/magnifyingGlass/index.vue"),
+    label: "",
   },
   {
     name: "展示父子组件渲染生命周期",
     Tag: ["js", "vue"],
-    path: "/lifecycle"
+    path: "/lifecycle",
+    component: () => import("@/view/overflowAuto.vue"),
+    label: "",
   },
   {
     name: "canvas粒子特效",
     Tag: ["js", "canvas"],
     path: "/canvasParticle",
-    component:"/view/canvasParticle/index.vue"
+    component: () => import("@/view/canvasParticle/index.vue"),
+    label: "",
+  },
+  {
+    name: "电子签名",
+    Tag: ["js", "canvas"],
+    path: "/canvasSignature",
+    component: () => import("@/view/canvasSignature/index.vue"),
+    label: "",
   }
 ]
