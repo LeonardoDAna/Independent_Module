888f1a4cbbe1ef9ea340ae53432536bb077d7873
leonardeDAna <943682226@qq.com>
2021-08-19 20:29:21 +0800

    生命周期

---
+++ canvasVerification/src/view/lifecycle/component/child.vue
@@ -0,0 +1,29 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        componentName: "子",
+      }
+    },
+    beforeCreate() {
+      console.log(`${this.componentName}组件 beforeCreate`);
+    },
+    created() {
+      console.log(`${this.componentName}组件 created`);
+    },
+    beforeMount() {
+      console.log(`${this.componentName}组件 beforeMount`);
+    },
+    mounted() {
+      console.log(`${this.componentName}组件 mounted`);
+    }
+  }
+</script>
+
+<style>
+
+</style>
