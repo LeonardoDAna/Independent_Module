c45217c82072a7de8a7e98ccd236d6f99a8fa5ec
LeonardoDAna <943682226@qq.com>
2021-08-23 17:28:02 +0800

    selectOption修改

--- canvasVerification/src/components/selectOption.vue
+++ canvasVerification/src/components/selectOption.vue
@@ -1,19 +1,36 @@
 <template>
   <!-- 下拉选择框 -->
   <div class="selectInput">
+    {{ selectedOne }}
     <input
+      style="width: 100%"
+      size="1"
       type="text"
       readonly="readonly"
       autocomplete="off"
       placeholder="请选择"
-      width="300"
-      :onfocus="inputOnFocus()"
+      :value="selectedOne"
+      @focus="inputOnFocus()"
+      @blur="inputOnBlur()"
     />
     <span class="arrowSpan">
       <span>
-        <i class="el-icon-arrow-up input__icon"></i>
+        <i
+          class="el-icon-arrow-up input__icon"
+          :class="{ is_reverse: isFocus }"
+        ></i>
       </span>
     </span>
+    <div class="optionsList" v-if="isFocus">
+      <div
+        class="selectOption"
+        v-for="(item, index) in selectOption"
+        :key="index"
+        @click="selectOptFn(item)"
+      >
+        {{ item.label }}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -24,17 +41,50 @@ export default {
     selectValue: String,
   },
   data() {
-    return {};
+    return {
+      isFocus: false,
+      selectedOne: null,
+    };
   },
   methods: {
+    selectOptFn(item) {
+      this.selectedOne = item.label;
+      console.log(this.selectedOne);
+    },
     inputOnFocus() {
-      console.log("选中");
+      this.isFocus = true;
+    },
+    inputOnBlur() {
+      this.isFocus = false;
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
+.optionsList {
+  position: absolute;
+  top: 40px;
+  box-sizing: border-box;
+  border-radius: 4px;
+  border: 1px solid #c8cccf;
+  width: 100%;
+  transition: ease-in-out 0.3s;
+}
+.selectOption {
+  cursor: pointer;
+  padding: 5px 10px;
+  color: #606266;
+}
+.selectOption:hover {
+  background-color: #f5f7fa;
+}
+.is_reverse {
+  transform: rotate(180deg);
+  transition: 0.3s;
+  color: #c0c4cc;
+  font-size: 14px;
+}
 .selectInput {
   width: 100%;
   position: relative;
@@ -42,17 +92,19 @@ export default {
 .arrowSpan {
   position: absolute;
   height: 100%;
+  font-size: 14px;
   right: 5px;
   top: 0;
   text-align: center;
   color: #c0c4cc;
-  transition: all 0.3s;
+  transform: rotate(0deg);
+  transition: transform 0.3s;
   pointer-events: none;
 }
-.arrowSpan:hover {
-  transform: rotate(180deg);
+.el-icon-arrow-up {
+  transition: transform 0.3s;
 }
-.input__icon{
+.input__icon {
   line-height: 30px;
 }
 </style>
